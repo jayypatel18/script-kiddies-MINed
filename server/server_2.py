4eb3cba60e122d7f1d0711452d3afd6e1a9c139f
jayypatel18 <jayrpatel1802@gmail.com>
2025-01-30 23:16:10 +0530

    modified port

--- server/server_2.py
+++ server/server_2.py
@@ -140,4 +140,4 @@ def test_local_pdfs():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
