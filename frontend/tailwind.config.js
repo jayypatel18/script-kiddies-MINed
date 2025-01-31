94ec63f74b1b325ccfcbfca2713a0c27a36f6839
jayypatel18 <jayrpatel1802@gmail.com>
2025-01-31 19:46:01 +0530

    fix for tailwindcss not supporting

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./src/**/*.{js,jsx,ts,tsx}", // Adjust the paths based on your project structure
-    ],
-    theme: {
-      extend: {}, // Use this to customize the default Tailwind theme
-    },
-    plugins: [], // Add plugins here if needed
-  };
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
